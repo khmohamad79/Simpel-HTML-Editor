1d2146094616245ad45ceddd356023eb23ebf398
Mohammad <m.khodnama@gmail.com>
2019-04-28 01:54:35 +0430

    completed LinkedList class prototype

--- azap6-2/LinkedList.h
+++ azap6-2/LinkedList.h
@@ -1,8 +1,19 @@
 #pragma once
-class LinkedList
+template <class N> class LinkedList
 {
+	unsigned int size;
+	N* head;
 public:
 	LinkedList();
+	LinkedList(const N & node)
 	~LinkedList();
+	bool addNode(const N & node);
+	bool addNode(const N & node, const unsigned int & index);
+	bool addNode(const N & node, N* previousNodeptr);
+	bool removeNode(const unsigned int & index);
+	bool removeNode(N* nodeptr);
+	N* getNode(const unsigned int & index) const;
+	N* getNode(N* previous) const;
+	unsigned int getSize() const;
 };
 
