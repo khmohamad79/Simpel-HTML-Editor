00ee76034991af16eae900ed64a875a362460d90
Mohammad <m.khodnama@gmail.com>
2019-04-29 06:25:10 +0430

    main cpp created

---
+++ azap6-2/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "LinkedList.cpp"
+
+using namespace std;
+
+int main()
+{
+	LinkedList<int> mylist;
+	mylist.addNode(1);
+	mylist.addNode(2);
+	for (int i = 0; i < mylist.getSize(); i++)
+	{
+		cout << mylist.getNodeData(i);
+	}
+	cout << endl;
+	while (mylist.getSize()>0)
+	{
+		cout << mylist.getNodeData(mylist.getSize() - 1);
+		mylist.removeNode(mylist.getSize() - 1);
+	}
+	return 0;
+}
