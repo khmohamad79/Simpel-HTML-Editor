df5a6ea4dbb6d99694a50bef3e9f8a3e839e3a22
Mohammad <m.khodnama@gmail.com>
2019-04-30 11:37:51 +0430

    Tag prototype completed.

--- azap6-2/Source.cpp
+++ azap6-2/Source.cpp
@@ -5,19 +5,23 @@ using namespace std;
 
 int main()
 {
-	LinkedList<int> mylist;
-	mylist.addNode(1);
-	mylist.addNode(2);
+	LinkedList<string> mylist;
+	mylist.addNode("1");
+	mylist.addNode("2");
 	for (int i = 0; i < mylist.getSize(); i++)
 	{
-		cout << mylist.getNodeData(i);
+		cout << mylist.getNodeData(i).c_str();
 	}
+	mylist.addNode("0", 2);
 	cout << endl;
-	mylist.addNode(0, 2);
+	for (int i = 0; i < mylist.getSize(); i++) cout << mylist.getNodeData(i).c_str();
+	cout << endl;
+	mylist.addNode("0", 1);
 	while (mylist.getSize()>0)
 	{
-		cout << mylist.getNodeData(mylist.getSize() - 1);
+		cout << mylist.getNodeData(mylist.getSize() - 1).c_str();
 		mylist.removeNode(mylist.getSize() - 1);
 	}
+	cout << mylist.getSize();
 	return 0;
 }
--- azap6-2/Tag.h
+++ azap6-2/Tag.h
@@ -1,8 +1,29 @@
 #pragma once
+#include "LinkedList.h"
+#include <iterator>
+#include <map>
+#include <string.h>
 class Tag
 {
+	std::map<std::string, std::string> attributes;
+	LinkedList<Tag> tags;
+	std::string text;
+	LinkedList<std::string> texts;
 public:
 	Tag();
 	~Tag();
+
+	void addTag(const Tag & tag);
+	void addText(const char* str);
+	void addTag(const Tag & tag, const unsigned int & index);
+	void addText(const char* str, const unsigned int & index);
+	void removeTag();
+	void removeText();
+	void removeTag(const unsigned int & index);
+	void removeText(const unsigned int & index);
+	Tag & getTag(const unsigned int & index);
+	std::string getText(const unsigned int & index);
+
+	friend std::ostream & operator<<(std::ostream & output, const Tag & tag);
 };
 
