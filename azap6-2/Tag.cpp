ad1d9710fc6c72a084991bf476a6208dfa070bf4
Mohammad <m.khodnama@gmail.com>
2019-04-30 14:28:55 +0430

    overrided ostream << for Tag.

--- azap6-2/Tag.cpp
+++ azap6-2/Tag.cpp
@@ -2,15 +2,10 @@
 
 
 
-Tag::Tag()
-{
-	text = "";
-}
-
-
 Tag::Tag(std::string tag_name)
 {
 	name = tag_name;
+	text = "";
 }
 
 Tag::~Tag()
@@ -135,4 +130,20 @@ std::string Tag::getText(const unsigned int & index) const
 std::string Tag::getAttrVal(const char * str_name) const
 {
 	return attributes.at(str_name);
+}.
+
+std::ostream & operator<<(std::ostream & output, const Tag & tag)
+{
+	output << "<" << tag.name << " ";
+	for (auto it = tag.attributes.cbegin(); it != tag.attributes.cend(); ++it)
+	{
+		output << it->first << "=" << it->second << " ";
+	}
+	output << ">";
+	output << tag.text;
+	for (int i = 0; i < tag.getSize(); i++)
+	{
+		output << tag.tags[i] << tag.texts[i];
+	}
+	output << "</" << tag.name << ">";
 }
