a072262e5a1f2d405dd61d9783d9178e28cb476b
Mohammad <m.khodnama@gmail.com>
2019-04-30 11:39:50 +0430

    Revert "Tag prototype completed."
    This reverts commit df5a6ea4dbb6d99694a50bef3e9f8a3e839e3a22.

--- azap6-2/Source.cpp
+++ azap6-2/Source.cpp
@@ -5,23 +5,19 @@ using namespace std;
 
 int main()
 {
-	LinkedList<string> mylist;
-	mylist.addNode("1");
-	mylist.addNode("2");
+	LinkedList<int> mylist;
+	mylist.addNode(1);
+	mylist.addNode(2);
 	for (int i = 0; i < mylist.getSize(); i++)
 	{
-		cout << mylist.getNodeData(i).c_str();
+		cout << mylist.getNodeData(i);
 	}
-	mylist.addNode("0", 2);
 	cout << endl;
-	for (int i = 0; i < mylist.getSize(); i++) cout << mylist.getNodeData(i).c_str();
-	cout << endl;
-	mylist.addNode("0", 1);
+	mylist.addNode(0, 2);
 	while (mylist.getSize()>0)
 	{
-		cout << mylist.getNodeData(mylist.getSize() - 1).c_str();
+		cout << mylist.getNodeData(mylist.getSize() - 1);
 		mylist.removeNode(mylist.getSize() - 1);
 	}
-	cout << mylist.getSize();
 	return 0;
 }
