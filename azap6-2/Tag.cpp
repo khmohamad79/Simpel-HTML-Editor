a6705ba8aa87e292f953d5b6cfba32b26f7f4395
Mohammad <m.khodnama@gmail.com>
2019-04-30 14:10:01 +0430

    updated prototype and declared methods in cpp.

--- azap6-2/Tag.cpp
+++ azap6-2/Tag.cpp
@@ -8,6 +8,11 @@ Tag::Tag()
 }
 
 
+Tag::Tag(std::string tag_name)
+{
+	name = tag_name;
+}
+
 Tag::~Tag()
 {
 }
@@ -24,3 +29,110 @@ void Tag::addText(const char * str)
 	else text = str;
 }
 
+void Tag::addAttr(const char * str_name, const char * str_value)
+{
+	attributes.insert({ str_name, str_value });
+}
+
+void Tag::insertTag(const Tag & tag, const unsigned int & index)
+{
+	tags.insert(tags.begin() + index, tag);
+	texts.insert(texts.begin() + index, "");
+}
+
+void Tag::insertText(const char * str, const unsigned int & index)
+{
+	if (index >= 0 && index < texts.size()) texts.at(index) = str;
+	else text = str;
+}
+
+void Tag::removeTag()
+{
+	removeTag(tags.size() - 1);
+}
+
+void Tag::removeText()
+{
+	if (texts.size() > 0) *(texts.end()) = "";
+	else text = "";
+}
+
+void Tag::removeTag(const unsigned int & index)
+{
+	if (index == 0) text += *(texts.begin());
+	else *(texts.at[index - 1]) += *(texts.at[index]);
+	tags.erase(tags.begin() + index);
+	texts.erase(texts.begin() + index);
+}
+
+void Tag::removeText(const unsigned int & index)
+{
+	if (index >= 0 && index < texts.size()) *(texts.begin() + index) = "";
+	else text = "";
+}
+
+void Tag::removeAttr(const char * str_name)
+{
+	attributes.erase(str_name);
+}
+
+void Tag::clearTags()
+{
+	while (tags.size() > 0)
+	{
+		removeTag();
+	}
+}
+
+void Tag::clearAttrs()
+{
+	attributes.clear();
+}
+
+void Tag::clear()
+{
+	clearTags();
+	clearAttrs();
+	removeText();
+}
+
+void Tag::updateTag(const Tag & tag, const unsigned int & index)
+{
+	tags.at(index) = tag;
+}
+
+void Tag::updateText(const char * str, const unsigned int & index)
+{
+	if (index >= 0 && index < texts.size()) texts.at(index) = str;
+	else text = str;
+}
+
+void Tag::updateAttr(const char * str_name, const char * str_value)
+{
+	attributes.at(str_name) = str_value;
+}
+
+unsigned int Tag::getSize() const
+{
+	return tags.size(); // or texts.size()
+}
+
+Tag & Tag::getTag(const unsigned int & index)
+{
+	return tags.at(index);
+}
+
+std::string Tag::getText() const
+{
+	return text;
+}
+
+std::string Tag::getText(const unsigned int & index) const
+{
+	return texts.at(index);
+}
+
+std::string Tag::getAttrVal(const char * str_name) const
+{
+	return attributes.at(str_name);
+}
