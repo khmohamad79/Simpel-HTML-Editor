0fdaa8e8400dee42dbfd188587647932b3d53ed2
Mohammad <m.khodnama@gmail.com>
2019-05-03 23:35:08 +0430

    created simple application of Tag and TagMenu.

--- azap6-2/Source.cpp
+++ azap6-2/Source.cpp
@@ -1,23 +1,35 @@
 #include <iostream>
 #include <fstream>
-#include "string"
-#include "Tag.h"
+#include <string>
+#include "PairedTag.h"
+#include "SingleTag.h"
+#include "TagMenu.h"
 
 using namespace std;
 
 int main()
 {
-	Tag* temp;
-	Tag body("body");
-	body.addText("before");
-	body.addTag(Tag("h1"));
-	body.addText("after");
-	temp = &body.getTag(0);
-	temp->addAttr("style", "color:red;");
-	temp->addText("Header1");
-	
-	fstream fp("index.html", ios::out);
-	fp << body;
-	//fp.close();
+	char file_name[81];
+	cout << "enter file name:";
+	cin.getline(file_name, 80);
+
+	cout << "*** back from body = save ***\n";
+	cout << "press any key to create html tag!";
+	cin.get();
+
+	fstream fp(strcat(file_name, ".html"), ios::out);
+	if (!fp) {
+		cout << endl;
+		cout << "problem opennig file!!\n";
+	}
+	else
+	{
+		PairedTag html("html");
+		showMenu(&html);
+		fp << html;
+		fp.close();
+		cout << endl;
+		cout << "file created successfully\n";
+	}
 	return 0;
 }
