2e91f4872896747b4c896bc36d864924046962b3
Mohammad <m.khodnama@gmail.com>
2019-05-03 12:22:25 +0430

    changed addText & editText & removeText.

--- azap6-2/Source.cpp
+++ azap6-2/Source.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
 #include <fstream>
-#include "string"
-#include "Tag.h"
+#include <string>
+#include "PairedTag.h"
+#include "SingleTag.h"
+#include "TagMenu.h"
 
 using namespace std;
 
 int main()
 {
-	Tag* temp;
-	Tag body("body");
-	body.addText("before");
-	body.addTag(Tag("h1"));
-	body.addText("after");
-	temp = &body.getTag(0);
-	temp->addAttr("style", "color:red;");
-	temp->addText("Header1");
 	
+	PairedTag body("body");
+	body.addText("start");
+	body.addTag(new PairedTag("h1"));
+	body.addTag(new SingleTag("a"));
+	body.addText("middle",0);
+
+	PairedTag* pt = (PairedTag*)body.getTag(1);
+	showMenu(&body);
+	//pt->addTag(new SingleTag("hr"));
+	//body.addTag(new PairedTag("p"));
+	//body.addTag(new SingleTag("a"));
+	//body.addText("end");
+
+	
+	//temp1->addAttr("style", "color:red;");
+	//temp1->addText("Header1");
+	
+	/*Tag* temp2 = &body.getTag(1);
+	temp2->addAttr("style", "color:blue;");
+	temp2->addText("parag");
+	*/
 	fstream fp("index.html", ios::out);
 	fp << body;
-	//fp.close();
+	fp.close();
+
+	cout << body <<endl;
+
 	return 0;
 }
--- azap6-2/TagMenu.cpp
+++ azap6-2/TagMenu.cpp
@@ -27,10 +27,12 @@ void showMenu(Tag * tag)
 	{
 		cout << ">" << endl;
 		PairedTag* ptag = (PairedTag*)tag;
+		cout << "   0.\"" << ptag->getText() << "\"\n";
 		for (int j = 0; j < c_tag; j++)
 		{
 			Tag* tempTag = ptag->getTag(j);
 			cout << "   " << (j + 1) << ".<" << ptag->getTag(j)->getName() << "/>\n";
+			cout << "   " << (j + 1) << ".\""  << ptag->getText(j) << "\"\n";
 		}
 		cout << "</" << tag->getName() << ">\n";
 	}
@@ -47,13 +49,14 @@ void showMenu(Tag * tag)
 	}
 	if (type == 2)
 	{
-		cout << "4.AddText\n";
+		cout << "4.AppendText\n";
 		cout << "5.EditText\n";
 		cout << "6.DeleteText\n";
 		cout << "7.AddTag\n";
 		cout << "8.OpenTag\n";
 		cout << "9.DeleteTag\n";
 	}
+	cout << endl;
 
 	int ch_command = 0;
 	if (type == 1) {
@@ -133,9 +136,102 @@ void showMenu(Tag * tag)
 			}
 		}
 	}
-	
-	
+	else if (ch_command == 4)
+	{
+		PairedTag* ptag = (PairedTag*)tag;
+		int ch_num;
+		do
+		{
+			cout << "Enter Text number to append(-1:Cancel):";
+			cin >> ch_num;
+		} while (ch_num < -1 || ch_num > c_tag);
+		if (ch_num > 0)
+		{
+			cout << ptag->getText(ch_num - 1);
+			char temp_str[81];
+			cin.getline(temp_str, 80);
+			ptag->addText(temp_str, ch_num - 1);
+		}
+		else if (ch_num == 0)
+		{
+			cout << ptag->getText();
+			char temp_str[81];
+			cin.getline(temp_str, 80);
+			ptag->addText(temp_str);
+		}
+	}
+	else if (ch_command == 5)
+	{
+		PairedTag* ptag = (PairedTag*)tag;
+		int ch_num;
+		do
+		{
+			cout << "Enter Text number to edit(-1:Cancel):";
+			cin >> ch_num;
+		} while (ch_num < -1 || ch_num > c_tag);
+		if (ch_num > 0)
+		{
+			char temp_str[81];
+			cin.getline(temp_str, 80);
+			ptag->updateText(temp_str, ch_num - 1);
+		}
+		else if (ch_num == 0)
+		{
+			char temp_str[81];
+			cin.getline(temp_str, 80);
+			ptag->updateText(temp_str);
+		}
+	}
+	else if (ch_command == 6)
+	{
+		PairedTag* ptag = (PairedTag*)tag;
+		int ch_num;
+		do
+		{
+			cout << "Enter Text number to delete(-1:Cancel):";
+			cin >> ch_num;
+		} while (ch_num < -1 || ch_num > c_tag);
+		if (ch_num > 0)
+		{
+			ptag->removeText(ch_num - 1);
+		}
+		else if (ch_num == 0)
+		{
+			ptag->removeText();
+		}
+	}
+	else if (ch_command == 7)
+	{
+
+	}
+	else if (ch_command == 8)
+	{
+		PairedTag* ptag = (PairedTag*)tag;
+		int ch_num;
+		do
+		{
+			cout << "Enter Tag number to open(0:Cancel):";
+			cin >> ch_num;
+		} while (ch_num < 0 || ch_num > c_tag);
+		if (ch_num > 0)
+		{
+			showMenu(ptag->getTag(ch_num - 1));
+		}
+	}
+	else if (ch_command == 9)
+	{
+		PairedTag* ptag = (PairedTag*)tag;
+		int ch_num;
+		do
+		{
+			cout << "Enter Tag number to delete(0:Cancel):";
+			cin >> ch_num;
+		} while (ch_num < 0 || ch_num > c_tag);
+		if (ch_num > 0)
+		{
+			ptag->removeTag(ch_num - 1);
+		}
+	}
 	
 	showMenu(tag);
-
 }
