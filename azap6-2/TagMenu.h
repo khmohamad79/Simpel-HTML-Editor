6116e562145e0e1cb745b86f324260820c03a573
Mohammad <m.khodnama@gmail.com>
2019-05-03 04:52:00 +0430

    changed TagMenu prototype.

--- azap6-2/TagMenu.h
+++ azap6-2/TagMenu.h
@@ -1,6 +1,9 @@
 #pragma once
-#include "Tag.h"
+#include <iostream>
+#include <conio.h>
+#include "SingleTag.h"
+#include "PairedTag.h"
 using namespace std;
-void showMenu(Tag & tag);
-void editTag(Tag & tag);
-Tag & createTag();
+void showMenu(Tag * tag);
+void editTag(Tag * tag);
+Tag * createTag();
